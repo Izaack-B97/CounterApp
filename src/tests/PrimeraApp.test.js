ad6167b203e69097cf317fa5d5435d66e9616a62
Isaac Bustamante <izaack.97@gmail.com>
2020-12-18 11:42:21 -0700

    test - enzyme

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import PrimeraApp from '../PrimeraApp';
+// import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 
+import PrimeraApp from '../PrimeraApp';
 
 describe('Pruebas en <PrimeraApp /> ', () => {
 
-    test('debe de mostrar el mensaje "Hola, soy Isaac"', () => {
-        
-        const saludo = 'Hola, soy Isaac';
-
-        const wrapper = render( <PrimeraApp saludo={ saludo }/> );
-        const { getByText } = wrapper;
+    // test('debe de mostrar el mensaje "Hola, soy Isaac"', () => {
+    //     const saludo = 'Hola, soy Isaac';
+    //     const wrapper = render( <PrimeraApp saludo={ saludo }/> );
+    //     const { getByText } = wrapper;
+    //     expect( getByText(saludo) ).toBeInTheDocument();
+    // });
     
-        expect( getByText(saludo) ).toBeInTheDocument();
-    });
+
+    test('should ', () => {
+        const saludo = 'Hola, soy Isaac';
+        const wrapper = shallow( <PrimeraApp saludo={ saludo }/> );
+
+        expect( wrapper ).toMatchSnapshot();        
+
+    })
     
 
 });
